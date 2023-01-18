e9e853c5478e161745f5652dc9d4be9323be90b9
Javlonbek2106 <122851751+Javlonbek2106@users.noreply.github.com>
2023-01-18 08:57:59 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OOP_9;
+using OOP_9._2_masala;
+
+/*Triangle object1= new Triangle(10,20,30);
+Triangle object2 = new Triangle();
+object2 =-object1;
+object2.GetABC();*/
+Minus m = new(10, 20, 30);
+//m =-m;
+//m.Minus_2();
+Minus n = new(5, 5, 5);
+m=m-n;
+m.Minus_2();
+Creature.Insects a = new(5);
+Console.WriteLine(a.Legs);
+
+
+
+
