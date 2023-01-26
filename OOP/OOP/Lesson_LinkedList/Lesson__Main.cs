4ca4bdb49f17c789783f0b9c768dd910975048e3
Javlonbek2106 <122851751+Javlonbek2106@users.noreply.github.com>
2023-01-26 14:57:08 +0500

    Add files via upload
    Signed-off-by: Javlonbek2106 <122851751+Javlonbek2106@users.noreply.github.com>

---
+++ OOP/OOP/Lesson_LinkedList/Lesson__Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Lesson_LinkedList
+{
+    internal class Lesson__Main
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
