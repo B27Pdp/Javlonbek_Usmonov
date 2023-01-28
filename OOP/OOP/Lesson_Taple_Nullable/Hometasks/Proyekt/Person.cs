d96a8117339445450391a118993f57d4e8f13296
Javlonbek2106 <122851751+Javlonbek2106@users.noreply.github.com>
2023-01-28 08:42:12 +0500

    Add files via upload
    Signed-off-by: Javlonbek2106 <122851751+Javlonbek2106@users.noreply.github.com>

---
+++ OOP/OOP/Lesson_Taple_Nullable/Hometasks/Proyekt/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Lesson_Taple_Nullable.Hometasks.Proyekt
+{
+    /*public class Person : IPerson<TId>
+    {
+        public TId Id { get; set; }
+        public byte age { get; set; }
+        public string name { get; set; }
+
+        public Person(TId id, byte age, string name)
+        {
+            Id = id;
+            this.age = age;
+            this.name = name;
+        }
+    }*/
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOP.Lesson_LinkedList;
+using OOP.Lesson_Taple_Nullable;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Linkedlist_Main.Main();
+            Tuple_Main.Mainn();
+            
+        }
+    }
+}
